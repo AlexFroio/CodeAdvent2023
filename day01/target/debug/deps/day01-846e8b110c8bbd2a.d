7befaa0077df9678aaf54349ae381a2c11b79ae5
AlexFroio <alexfroio42@gmail.com>
2023-12-10 22:03:48 -0500

    Delete day01/target directory

--- day01/target/debug/deps/day01-846e8b110c8bbd2a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\alexa\Documents\Code Projects\CodeAdvent2023\day01\target\debug\deps\libday01-846e8b110c8bbd2a.rmeta: src\main.rs
-
-C:\Users\alexa\Documents\Code Projects\CodeAdvent2023\day01\target\debug\deps\day01-846e8b110c8bbd2a.d: src\main.rs
-
-src\main.rs:
--- day01/target/debug/deps/utils-9c9b9bab720b4a13.d
+++
@@ -1,7 +0,0 @@
-C:\Users\alexa\Documents\Code Projects\CodeAdvent2023\day01\target\debug\deps\libutils-9c9b9bab720b4a13.rmeta: C:\Users\alexa\Documents\Code\ Projects\CodeAdvent2023\utils\src\lib.rs
-
-C:\Users\alexa\Documents\Code Projects\CodeAdvent2023\day01\target\debug\deps\libutils-9c9b9bab720b4a13.rlib: C:\Users\alexa\Documents\Code\ Projects\CodeAdvent2023\utils\src\lib.rs
-
-C:\Users\alexa\Documents\Code Projects\CodeAdvent2023\day01\target\debug\deps\utils-9c9b9bab720b4a13.d: C:\Users\alexa\Documents\Code\ Projects\CodeAdvent2023\utils\src\lib.rs
-
-C:\Users\alexa\Documents\Code\ Projects\CodeAdvent2023\utils\src\lib.rs:
